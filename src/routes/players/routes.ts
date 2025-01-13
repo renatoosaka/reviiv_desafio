0d33934275022ac69d7d030807112b6a0e9c50fc
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 20:23:29 -0300

    feat: add list players route

---
+++ src/routes/players/routes.ts
@@ -0,0 +1,21 @@
+import { createRoute, z } from "@hono/zod-openapi";
+
+import { HTTP_STATUS } from "@/lib/constants";
+
+export const list = createRoute({
+  tags: ["Players"],
+  method: "get",
+  path: "/players",
+  summary: "List of Players",
+  responses: {
+    [HTTP_STATUS.OK.CODE]: {
+      description: "List of Players",
+      content: {
+        "application/json": {
+          schema: z.array(z.string()),
+          example: ["player_1", "player_2"],
+        },
+      },
+    },
+  },
+});
