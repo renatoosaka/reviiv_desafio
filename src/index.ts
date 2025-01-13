f3e844f53e1a6f14e80018699d33c51f57cf13ff
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 18:37:50 -0300

    feat: load port from env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { app } from "@/app";
 import { serve } from "@hono/node-server";
+
+import { app } from "@/app";
+import env from "@/env";
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-const port = 3000;
+const port = env.PORT;
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
