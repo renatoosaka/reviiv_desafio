2239ca1d91b85ba568268f78c68884ae6fc551f6
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 19:22:16 -0300

    feat: add get one game route

--- src/routes/games/routes.ts
+++ src/routes/games/routes.ts
@@ -1,7 +1,7 @@
-import { createRoute } from "@hono/zod-openapi";
+import { createRoute, z } from "@hono/zod-openapi";
 
 import { HTTP_STATUS } from "@/lib/constants";
-import { gamesSchema } from "@/routes/games/schema";
+import { gameSchema, gamesSchema } from "@/routes/games/schema";
 
 export const list = createRoute({
   tags: ["Games"],
@@ -30,4 +30,59 @@ export const list = createRoute({
   },
 });
 
+export const getOne = createRoute({
+  tags: ["Games"],
+  method: "get",
+  path: "/games/{id}",
+  summary: "Get a Game",
+  request: {
+    params: z.object({
+      id: z.string().openapi({
+        param: {
+          name: "id",
+          in: "path",
+          required: true,
+        },
+        required: ["id"],
+        example: "game_1",
+      }),
+    }),
+  },
+  responses: {
+    [HTTP_STATUS.OK.CODE]: {
+      description: "Game",
+      content: {
+        "application/json": {
+          schema: gameSchema,
+        },
+      },
+    },
+    [HTTP_STATUS.NOT_FOUND.CODE]: {
+      description: "Game not found",
+      content: {
+        "application/json": {
+          schema: z.object({
+            message: z.string().openapi({
+              example: "Game not found",
+            }),
+          }),
+        },
+      },
+    },
+    [HTTP_STATUS.UNPROCESSABLE_ENTITY.CODE]: {
+      description: "Invalid ID",
+      content: {
+        "application/json": {
+          schema: z.object({
+            message: z.string().openapi({
+              example: "Invalid ID",
+            }),
+          }),
+        },
+      },
+    },
+  },
+});
+
 export type ListRoute = typeof list;
+export type GetOneRoute = typeof getOne;
