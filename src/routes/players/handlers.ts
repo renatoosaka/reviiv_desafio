41ac4e8bb4bfa61b50b21d28cb2b916c2d49ff34
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 20:28:34 -0300

    feat: add list player games handler

--- src/routes/players/handlers.ts
+++ src/routes/players/handlers.ts
@@ -1,6 +1,8 @@
 import type { AppRouteHandler } from "@/lib/types";
-import type { ListRoute } from "@/routes/players/routes";
+import type { GameSchemaType } from "@/routes/games/schema";
+import type { GamesRoute, ListRoute } from "@/routes/players/routes";
 
+import { HTTP_STATUS } from "@/lib/constants";
 import data from "@/parser/games.json" with { type: "json" };
 
 function getPlayers() {
@@ -18,4 +20,23 @@ function getPlayers() {
 export const list: AppRouteHandler<ListRoute> = (c) => {
   const players = getPlayers();
   return c.json(players);
+};
+
+export const games: AppRouteHandler<GamesRoute> = (c) => {
+  const { player_name } = c.req.valid("param");
+  const players = getPlayers();
+
+  if (!players.includes(player_name)) {
+    return c.json({ message: HTTP_STATUS.NOT_FOUND.MESSAGE }, HTTP_STATUS.NOT_FOUND.CODE);
+  }
+
+  const games = data.games;
+  const playerGames = Object.entries(games)
+    .filter(([, game]) => game.players.includes(player_name))
+    .reduce<Record<string, GameSchemaType>>((acc, [id, game]) => {
+      acc[id] = game;
+      return acc;
+    }, {});
+
+  return c.json(playerGames, HTTP_STATUS.OK.CODE);
 };
