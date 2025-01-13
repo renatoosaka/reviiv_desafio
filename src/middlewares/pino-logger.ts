100424dc1e5c98dc2f3017a69873e67ef5cd14b1
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 18:37:19 -0300

    feat: improve pino logger middleware

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,3 +1,4 @@
+import env from "@/env";
 import { pinoLogger as logger } from "hono-pino";
 import { randomUUID } from "node:crypto";
 import pino from "pino";
@@ -6,8 +7,8 @@ import pretty from "pino-pretty";
 export function pinoLogger() {
   return logger({
     pino: pino({
-      level: "debug",
-    }, pretty()),
+      level: env.LOG_LEVEL || "info",
+    }, env.NODE_ENV === "production" ? undefined : pretty()),
     http: {
       reqId: () => randomUUID(),
     },
