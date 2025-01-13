27033367c3e3cde924020b30bf66bbcd5ebb91b9
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 20:27:30 -0300

    feat: add get player games route

--- src/routes/players/routes.ts
+++ src/routes/players/routes.ts
@@ -1,6 +1,7 @@
 import { createRoute, z } from "@hono/zod-openapi";
 
 import { HTTP_STATUS } from "@/lib/constants";
+import { gameSchema } from "@/routes/games/schema";
 
 export const list = createRoute({
   tags: ["Players"],
@@ -20,4 +21,70 @@ export const list = createRoute({
   },
 });
 
+export const games = createRoute({
+  tags: ["Players"],
+  method: "get",
+  path: "/players/{player_name}/games",
+  summary: "List of Games for a Player",
+  request: {
+    params: z.object({
+      player_name: z.string().openapi({
+        param: {
+          name: "player_name",
+          in: "path",
+          required: true,
+        },
+        required: ["player_name"],
+        example: "player_1",
+      }),
+    }),
+  },
+  responses: {
+    [HTTP_STATUS.OK.CODE]: {
+      description: "List of Games",
+
+      content: {
+        "application/json": {
+          schema: z.record(z.string(), gameSchema),
+          example: {
+            game_1: {
+              total_kills: 10,
+              players: ["Isgalamido", "Mocinha"],
+              kills: {
+                Isgalamido: -7,
+                Mocinha: 0,
+              },
+            },
+          },
+        },
+      },
+    },
+    [HTTP_STATUS.NOT_FOUND.CODE]: {
+      description: "Player not found",
+      content: {
+        "application/json": {
+          schema: z.object({
+            message: z.string().openapi({
+              example: "Player not found",
+            }),
+          }),
+        },
+      },
+    },
+    [HTTP_STATUS.UNPROCESSABLE_ENTITY.CODE]: {
+      description: "Invalid player name",
+      content: {
+        "application/json": {
+          schema: z.object({
+            message: z.string().openapi({
+              example: "Invalid player name",
+            }),
+          }),
+        },
+      },
+    },
+  },
+});
+
 export type ListRoute = typeof list;
+export type GamesRoute = typeof games;
