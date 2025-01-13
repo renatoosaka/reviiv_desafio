b3e2f0461413f0d7fe832a28904271b6838f2944
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 18:57:44 -0300

    feat: add app bindings type

--- src/lib/hono-app.ts
+++ src/lib/hono-app.ts
@@ -1,12 +1,14 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
+import type { AppBindings } from "@/lib/types";
+
 import { defaultHook } from "@/middlewares/default-hook";
 import { notFound } from "@/middlewares/not-found";
 import { onError } from "@/middlewares/on-error";
 import { pinoLogger } from "@/middlewares/pino-logger";
 
 export function createRouter() {
-  return new OpenAPIHono({
+  return new OpenAPIHono<AppBindings>({
     strict: false,
     defaultHook,
   });
