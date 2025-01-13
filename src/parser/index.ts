cb01e2590197e4662b3cd80fd981cb4c0918d46d
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 18:46:12 -0300

    feat: add transform stream

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,11 +1,27 @@
 /* eslint-disable no-console */
 import fs from "node:fs";
 import path from "node:path";
-import { pipeline, Readable } from "node:stream";
+import { pipeline, Readable, Transform } from "node:stream";
 import url from "node:url";
+
+const INITGAME = "InitGame:";
+const KILL = "Kill:";
+const USER = "ClientUserinfoChanged:";
+const WORLD = "<world>";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const games: Record<string, any> = {};
+const currentGame: Record<string, any> = {};
+
+const transformStream = new Transform({
+  transform(chunk, _, callback) {
+    const line = chunk.toString().trim();
+    this.push(line);
+    callback();
+  },
+});
 
 if (fs.existsSync(path.join(__dirname, "games.json"))) {
   fs.unlinkSync(path.join(__dirname, "games.json"));
@@ -17,7 +33,7 @@ const readableStream = Readable.from(
   }),
 );
 
-pipeline(readableStream, process.stdout, (err) => {
+pipeline(readableStream, transformStream, process.stdout, (err) => {
   if (err) {
     console.error("Pipeline failed", err);
   }
