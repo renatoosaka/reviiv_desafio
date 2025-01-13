e7ac18b04014905cb245c92b8d21db8ab94a87f3
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 19:14:18 -0300

    feat: add app route handler type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-import type { OpenAPIHono } from "@hono/zod-openapi";
+import type { OpenAPIHono, RouteConfig, RouteHandler } from "@hono/zod-openapi";
 import type { PinoLogger } from "hono-pino";
 
 export type AppBindings = {
@@ -8,3 +8,4 @@ export type AppBindings = {
 };
 
 export type AppOpenAPI = OpenAPIHono<AppBindings>;
+export type AppRouteHandler<R extends RouteConfig> = RouteHandler<R, AppBindings>;
