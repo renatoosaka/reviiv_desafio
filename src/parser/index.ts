cbfb4b83ff5d78764c9cba24f80dcaab5538e06b
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-13 18:44:20 -0300

    feat: delete parsed game file if exists

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import fs from "node:fs";
 import path from "node:path";
 import { pipeline, Readable } from "node:stream";
@@ -5,6 +6,10 @@ import url from "node:url";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+if (fs.existsSync(path.join(__dirname, "games.json"))) {
+  fs.unlinkSync(path.join(__dirname, "games.json"));
+}
 
 const readableStream = Readable.from(
   fs.createReadStream(path.join(__dirname, "games.log"), {
