c61b6f58e06a405184f2a190cd8637dc72486d2e
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-14 11:47:07 -0300

    feat: silent logger on test

--- src/middlewares/pino-logger.ts
+++ src/middlewares/pino-logger.ts
@@ -1,13 +1,14 @@
-import env from "@/env";
 import { pinoLogger as logger } from "hono-pino";
 import { randomUUID } from "node:crypto";
 import pino from "pino";
 import pretty from "pino-pretty";
 
+import env from "@/env";
+
 export function pinoLogger() {
   return logger({
     pino: pino({
-      level: env.LOG_LEVEL || "info",
+      level: env.NODE_ENV === "test" ? "silent" : env.LOG_LEVEL || "info",
     }, env.NODE_ENV === "production" ? undefined : pretty()),
     http: {
       reqId: () => randomUUID(),
