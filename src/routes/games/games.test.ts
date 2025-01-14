39aef587f9ba1f52f14123279d110e0708e93ea4
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-14 10:10:33 -0300

    feat: add games test

--- src/routes/games/games.test.ts
+++ src/routes/games/games.test.ts
@@ -1,7 +1,37 @@
+import { testClient } from "hono/testing";
 import { describe, expect, it } from "vitest";
 
+import { HTTP_STATUS } from "@/lib/constants";
+import { createApp } from "@/lib/hono-app";
+
+import { router } from ".";
+
+const client = testClient(createApp().route("/", router));
+
 describe("/games", () => {
-  it("shoud be ok", () => {
-    expect(1).toBe(1);
+  it("should list all games", async () => {
+    const response = await client.games.$get();
+
+    expect(response.status).toBe(HTTP_STATUS.OK.CODE);
+  });
+
+  it("should bring one specific game (game_1)", async () => {
+    const response = await client.games[":id"].$get({
+      param: {
+        id: "game_1",
+      },
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.OK.CODE);
+  });
+
+  it("should return 404 when get an inexistent game id", async () => {
+    const response = await client.games[":id"].$get({
+      param: {
+        id: "game_999",
+      },
+    });
+
+    expect(response.status).toBe(HTTP_STATUS.NOT_FOUND.CODE);
   });
 });
