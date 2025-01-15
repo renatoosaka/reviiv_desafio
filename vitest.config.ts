8bdda257fdc3d5c710d96b6f053dd952a988d020
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-15 11:23:43 -0300

    test: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
 import path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     coverage: {
+      reporter: ["text", "lcov", "html"],
       provider: "istanbul", // or 'v8'
     },
   },
