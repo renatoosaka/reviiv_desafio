26af45f997c19cbbe91a440f2eba33f3dcef6166
Renato Hideki Osaka <renato.osaka@hotmail.com>
2025-01-15 11:09:36 -0300

    test: add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    coverage: {
+      provider: "istanbul", // or 'v8'
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
